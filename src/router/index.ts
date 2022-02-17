3d099803ac37c60e9249e3660f31e1007e1fba9a
solonskiy <solonskiyigor@gmail.com>
2022-02-17 20:00:55 +0200

    added redirect for not found page

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,26 +6,32 @@ Vue.use(VueRouter)
 const routes: Array<RouteConfig> = [
     {
         path: '/',
-        redirect: '/dashboard'
+        redirect: '/manipulation'
     },
     {
         path: '/entrepreneurs',
         name: 'Entrepreneurs',
-        meta:{layout:"main-layout"},
+        meta: {layout: "main-layout"},
         component: () => import("../views/Entrepreneurs.vue")
     },
     {
         path: '/dashboard',
         name: 'Dashboard',
-        meta:{layout:"main-layout"},
+        meta: {layout: "main-layout"},
         component: () => import("../views/Dashboard.vue")
     },
     {
         path: '/manipulation',
         name: 'FileManipulation',
-        meta:{layout:"main-layout"},
+        meta: {layout: "main-layout"},
         component: () => import("../views/FileManipulation.vue")
     },
+    {
+        path: '/:pathMatch(.*)*',
+        name: 'not-found',
+        meta: {layout: "main-layout"},
+        component: () => import("../views/FileManipulation.vue")
+    }
 ]
 
 const router = new VueRouter({
